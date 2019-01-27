d40de878c4b5946d24a36131f51b4dce86a6b959
Mukesh Srivastava <mukeshsrivastav691@gmail.com>
2019-01-27 17:58:16 +0530

    - Assignment-3:  Add comment form component in DishDetailComponent.

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem,
+	Button, Modal, ModalHeader, ModalBody, Label, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
+	const maxLength = (len) => (val) => !(val) || (val.length <= len);
+	const minLength = (len) => (val) => val && (val.length >= len);
 
 	const RenderDish = ({dish}) => {
 		return (
@@ -37,6 +41,7 @@ import { Link } from 'react-router-dom';
 					<ul className="list-unstyled">
 						{commentItems}
 					</ul>
+					<CommentForm />
 				</div>
 			);
 		} else {
@@ -44,7 +49,112 @@ import { Link } from 'react-router-dom';
 		}
 	};
 
-	const DishDetail = (props) => {
+class CommentForm extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state= {
+			isModalOpen: false
+		};
+
+		this.toggleModal = this.toggleModal.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	toggleModal() {
+		this.setState({
+			isModalOpen: !this.state.isModalOpen
+		});
+	};
+
+	handleSubmit(values) {
+        this.toggleModal();
+
+        console.log('Current State is:', values);
+        alert('Current State is:' + JSON.stringify(values));
+
+    }
+
+	render() {
+		return (
+            <div>
+                <Button outline onClick={this.toggleModal}>
+                    <span className="fa fa-pencil fa-lg"></span> Submit Comment
+                </Button>
+
+                    <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                        <ModalHeader toggle={this.toggleModal}> Submit Comment</ModalHeader>
+                        <ModalBody>
+                                <LocalForm onSubmit={(values) => this.handleSubmit(values)} >
+                                    <Row className="form-group">
+                                    	<Label htmlFor="rating"  md={12}>Rating</Label>
+                                        <Col  md={12}>
+                                            <Control.select
+                                            	model=".rating"
+                                            	name="rating"
+                                            	className="form-control"
+                                             >
+                                                <option>1</option>
+                                                <option>2</option>
+                                                <option>3</option>
+                                                <option>4</option>
+                                                <option>5</option>
+                                            </Control.select>
+                                        </Col>
+                                    </Row>
+
+                                    <Row className="form-group">
+                                    	<Label htmlFor="author"  md={12}>Your Name</Label>
+                                        <Col  md={12}>
+                                            <Control.text 
+                                            	model=".author"
+                                            	id="author"
+                                            	name="author"
+                                            	placeholder="Your Name"
+                                            	className="form-control"
+                                            	validators={{ minLength:  minLength(3), maxLength: maxLength(15)}}
+                                             />
+                                            <Errors 
+                                            	className="text-danger"
+                                            	model=".author"
+                                            	show="touched"
+                                            	messages={{
+                                            		minLength: 'Must be greater than 2 characters',
+                                            		maxLength: 'Must be 15 charaters or less'
+                                            	}}
+                                             />
+                                        </Col>
+                                    </Row>
+
+                                    <Row className="form-group">
+                                    	<Label htmlFor="feedback" md={12}>Comment</Label>
+                                        <Col md={12}>
+                                            <Control.textarea 
+                                            	model=".comment"
+                                            	id="comment" 
+                                            	name="comment"
+                                            	rows="6"
+                                            	className="form-control"
+                                             />
+                                            <Errors 
+                                            	className="text-danger" 
+                                            	model=".comment"
+                                            	show="touched"
+                                            />
+                                        </Col>
+                                    </Row>
+
+                                    <Button type="submit" value="submit" color="primary">Submit</Button>
+                                </LocalForm>
+                        </ModalBody>
+                    </Modal>
+                
+            </div>
+		);
+	}
+}
+
+const DishDetail = (props) => {
 
 		if (props.dish!=null) {
 			return (
@@ -75,6 +185,5 @@ import { Link } from 'react-router-dom';
 			);
 		}
 	};
-
 
 export default DishDetail;
